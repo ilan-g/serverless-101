a313ef26d33bec4ba7458e567f99413ec5dc074b
Ilanchezhian <ilan-g@users.noreply.github.com>
2019-03-15 19:38:54 +0530

    calculator function

---
+++ lambda/calculator.js
@@ -0,0 +1,50 @@
+console.log('Loading the Calc function');
+
+exports.handler = function(event, context, callback) {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    
+    console.log('event.a:', event.a);
+    console.log('event.b:', event.b);
+    console.log('event.op:', event.op);
+    if (event.a === undefined || event.b === undefined || event.op === undefined) {
+        callback("400 Invalid Input");
+    }
+    
+    var res = {};
+    res.a = Number(event.a);
+    res.b = Number(event.b);
+    res.op = event.op;
+    
+     console.log('res:', res);
+     console.log('res.a:', res.a);
+     console.log('res.b:', res.b);
+     console.log('res.op:', res.op);
+    
+    if (isNaN(event.a) || isNaN(event.b)) {
+        callback("400 Invalid Operand  ");
+    }
+
+    switch(event.op)
+    {
+        case "+":
+        case "add":
+            res.c = res.a + res.b;
+            break;
+        case "-":
+        case "sub":
+            res.c = res.a - res.b;
+            break;
+        case "*":
+        case "mul":
+            res.c = res.a * res.b;
+            break;
+        case "/":
+        case "div":
+            res.c = res.b===0 ? NaN : Number(event.a) / Number(event.b);
+            break;
+        default:
+            callback("400 Invalid Operator");
+            break;
+    }
+    callback(null, res);
+};
